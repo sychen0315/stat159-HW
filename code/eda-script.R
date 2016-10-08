7afebc478c5fc727a7937580865145f3bcfc880a
sychen0315 <sychen0315@gmail.com>
2016-10-07 23:03:53 -0700

    add more to report

--- code/eda-script.R
+++ code/eda-script.R
@@ -1,9 +1,19 @@
+# Read data
 advertising <- read.csv("../data/Advertising.csv", sep = ",", row.names = 1)
 
+# Get summary of data and export
+tvsummary <- summary(advertising$TV)
+salessummary <- summary(advertising$Sales)
+sum <- matrix(ncol = 6, nrow = 2)
+sum[1,] <- tvsummary
+sum[2,] <- salessummary
+rownames(sum) <- c('TV', 'Sales')
+colnames(sum) <- names(salessummary)
 sink(file = "../data/eda-output.txt")
-summary(advertising)
+sum
 sink()
 
+# Graph output
 png(filename = "../images/histogram-tv.png")
 hist(advertising$TV, main = "Histogram of TV advertising budgets")
 dev.off()
